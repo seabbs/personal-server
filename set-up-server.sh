29e2265947406f1da319350b1bf39f7c3cde1ff1
Sam Abbott <s.e.abbott12@gmail.com>
2018-04-30 08:02:49 +0100

    linking changes

--- set-up-server.sh
+++ set-up-server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+## Get secrets from google cloud containers
+bash get_from_bucket.sh
 ## Create a docker network to link containers
 docker network create shinyproxy-net
-## Pull all required images
+## Pull all required images - errors are from containers that need to be built
 docker-compose -f containers/docker-compose.yml pull --ignore-pull-failures
 ## Build all custom images
 docker-compose -f containers/docker-compose.yml build
