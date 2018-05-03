3f86691d6869b565885e15edc9d8ca46ed67cdeb
Sam Abbott <s.e.abbott12@gmail.com>
2018-05-04 00:16:24 +0100

    push updates

--- push_to_bucket.sh
+++ push_to_bucket.sh
@@ -2,6 +2,6 @@
 
 ## Sync all sensitive/chanagable data
 ## Requires gcloud utils and a bucket to be set up at this location
-gsutil -m rsync -d -r secrets  gs://personal-server-bucket/secrets
+gsutil -m rsync -d -r secrets gs://personal-server-bucket/secrets
 gsutil -m rsync -d -r public_projects  gs://personal-server-bucket/public_projects
 gsutil -m rsync -d -r private_projects  gs://personal-server-bucket/private_projects
